484834a7ff225d94003039625726192ab216c5dd
Dale Nicholls <dlnchlls@gmail.com>
2021-08-25 19:08:00 +0100

    Add project files.

---
+++ AutoReadIn/AutoReadIn.cs
@@ -0,0 +1,12 @@
+﻿using Tekla.Structures.Model.Operations;
+
+namespace Tekla.Technology.Akit.UserScript
+{
+    static class Script
+    {
+        public static void Run(Tekla.Technology.Akit.IScript akit)
+        {
+            Operation.RunMacro("ReadIn.cs");
+        }
+    }
+}
